0530e72c92d5be217481574356192adb4533e039
Lydia Cupery <lac26@kernighan.cs.calvin.edu>
2016-12-06 21:39:52 -0500

    updated to show multiple components on one page

--- project/WICsite/app/scripts/index.js
+++ project/WICsite/app/scripts/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, Redirect, browserHistory } from 'react-router';
+import { Router, Route, Redirect, browserHistory, IndexRoute } from 'react-router';
 
 import EventBox from './eventBox';
 import EventEdit from './eventEdit';
+import EventList from './eventList';
 import App from './App.js';
+import Home from './Home.js';
 
 import '../css/base.css';
 
 ReactDOM.render((
     <Router history={browserHistory}>
-        <Route path="/" component={App}/>
-        <Route path="/:id" component={EventEdit} />
+        <Route path="/" component={App}>
+         <IndexRoute component={Home}/>
+        <Route path="/about" component={EventEdit} />
+        <Route path="/repos" component={EventBox} />
+        </Route>
     </Router>
 ), document.getElementById('content')
 );
