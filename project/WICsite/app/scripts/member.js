66e06a00cbcd98e728463b98d59dc5799d9572d3
Paige <plb7@students.calvin.edu>
2016-12-09 11:26:58 -0500

    added member components

---
+++ project/WICsite/app/scripts/member.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Remarkable from 'remarkable';
+import { Link } from 'react-router';
+
+module.exports = React.createClass({
+    rawMarkup: function() {
+        var md = new Remarkable({html: true});
+        var rawMarkup = md.render(this.props.children.toString());
+        return { __html: rawMarkup };
+    },
+    render: function() {
+        return (
+            <div className="memebr">
+                <h2 className="memberName" >
+                    {this.props.name}
+                </h2>
+                <span dangerouslySetInnerHTML={this.rawMarkup()} />
+		<Link to={'/members/' + this.props.id}>Edit</Link>
+            </div>
+        );
+    }
+});
---
+++ project/WICsite/app/scripts/memberEdit.js
@@ -0,0 +1,112 @@
+import React from 'react';
+import { Link } from 'react-router';
+import $ from 'jquery';
+
+import { API_URL } from './global';
+
+module.exports = React.createClass({
+    getInitialState: function() {
+        return {name: '', email: '', year: '', major: '',  role: ''};
+    },
+    componentDidMount: function() {
+        this.loadData();
+    },
+    componentDidUpdate: function(prevProps) {
+        if (this.props.params.id != prevProps.params.id) {
+            this.loadData();
+        }
+    },
+    loadData: function() {
+        $.ajax(API_URL + "/" + this.props.params.id) .done(function(members) {
+            this.setState(members[0]);
+        }.bind(this));
+    },
+    handleNameChange: function(e) {
+        this.setState({name: e.target.value});
+    },
+    handleEmailChange: function(e) {
+        this.setState({email: e.target.value});
+    },
+    handleYearChange: function(e) {
+        this.setState({year: e.target.value});
+    },
+    handleMajorChange: function(e) {
+        this.setState({major: e.target.value});
+    },
+    handleRoleChange: function(e) {
+        this.setState({role: e.target.value});
+    },
+    contextTypes: {
+        router: React.PropTypes.object
+    },
+    handleUpdate: function() {
+    var updatedMember = {
+        name: this.state.name.trim(),
+        email: this.state.email.trim(),
+        year: this.state.year.trim(),
+        major: this.state.major.trim(),
+        role: this.state.role.trim(),
+    };
+        $.ajax({
+            url: API_URL + "/" + this.props.params.id,
+            dataType: 'json',
+            type: 'PUT',
+            contentType:'application/json',
+            data: JSON.stringify(updatedMember)
+        })
+         .done(function(members){
+             this.context.router.push('/members');
+         }.bind(this))
+         .fail(function(xhr, status, errorThrown) {
+             console.error(API_URL, status, errorThrown.toString());
+         }.bind(this));
+    },
+    handleDelete: function() {
+        $.ajax({
+            url: API_URL + "/" + this.props.params.id,
+            type: 'DELETE',
+        })
+         .done(function(members){
+            this.context.router.push('/members');
+         }.bind(this))
+         .fail(function(xhr, status, errorThrown) {
+             console.error(API_URL, status, errorThrown.toString());
+         }.bind(this));
+    },
+    render: function() {
+        return (
+            <div>
+                <form className="memberForm">
+                    <h1>Member Edit - {this.state.id}</h1>
+                    <input
+                        type="text"
+                        value={this.state.name}
+                        onChange={this.handleNameChange}
+                    />
+                    <input
+                        type="text"
+                        value={this.state.email}
+                        onChange={this.handleEmailChange}
+                    /><input
+                        type="text"
+                        value={this.state.year}
+                        onChange={this.handleYearChange}
+                    />
+                    <input
+                        type="text"
+                        value={this.state.major}
+                        onChange={this.handleMajorChange}
+                    />
+                    <input
+                        type="text"
+                        value={this.state.role}
+                        onChange={this.handleRoleChange}
+                    />
+                    <button type="button" onClick={this.handleUpdate}>Update</button>
+                    <button type="button" onClick={this.handleDelete}>Delete</button>
+                </form>
+                <Link to='/'>Cancel</Link>
+            </div>
+        );
+    }
+});
