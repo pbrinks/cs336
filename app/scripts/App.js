ca9b72ce51504d86777c2ccd9fe4f5dac3523117
Lydia Cupery <lac26@noyce-kilby.cs.calvin.edu>
2016-12-17 11:34:01 -0500

    updated css

--- app/scripts/App.js
+++ app/scripts/App.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import NavLink from './NavLink'
+import base from '../css/base.css'
 
 export default React.createClass({
   render() {
     return (
       <div>
-        <h1>React Router Tutorial</h1>
+        <h1 className={base.pageHeader}>Calvin College Women in Computing</h1>
         <ul role="nav">
           <li><NavLink to="/" onlyActiveOnIndex>Home</NavLink></li>
           <li><NavLink to="/about">About</NavLink></li>
