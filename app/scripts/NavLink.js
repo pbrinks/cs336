9b097370b7b0c07da8b17bdef838d156276cc5ad
Lydia Cupery <lac26@leibniz.cs.calvin.edu>
2016-12-19 09:45:13 -0500

    documentation

--- app/scripts/NavLink.js
+++ app/scripts/NavLink.js
@@ -1,4 +1,8 @@
-// modules/NavLink.js
+/* Authors: Paige Brinks (plb7), Lydia Cupery (lac26)
+ * Date: December 19, 2016
+ *
+ * NavLink.js is used to wrap links to apply active style 
+ */
 import React from 'react'
 import { Link } from 'react-router'
 
