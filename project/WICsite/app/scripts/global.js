7bee761580bf7a56671c46ef48308437c6915d1d
Paige <plb7@students.calvin.edu>
2016-12-19 11:57:08 -0500

    now queries to only show future dates

--- project/WICsite/app/scripts/global.js
+++ project/WICsite/app/scripts/global.js
@@ -5,6 +5,6 @@
  
 const API_URL = "/api/events";
 const MEMBER_URL = "/api/members"
-const POLL_INTERVAL = 60000;
+const POLL_INTERVAL = 2000;
 
 module.exports = {API_URL, MEMBER_URL, POLL_INTERVAL};
